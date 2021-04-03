821f4365561ca3a5ed6d0f9af9f971a27d7b8723
ushprakash <ushaprakash1116@gmail.com>
2021-04-03 23:17:46 +0200

    Updates

---
+++ precache-manifest.13a2e35e7e509e089f1787fc88998aea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd427325614b828d6dc322fa73d003ab",
+    "url": "/React-TodoApp/index.html"
+  },
+  {
+    "revision": "da510de7e417b83479ec",
+    "url": "/React-TodoApp/static/css/main.8aa23f47.chunk.css"
+  },
+  {
+    "revision": "8448733c49b45e09a2f5",
+    "url": "/React-TodoApp/static/js/2.31583f47.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-TodoApp/static/js/2.31583f47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da510de7e417b83479ec",
+    "url": "/React-TodoApp/static/js/main.bb48ea12.chunk.js"
+  },
+  {
+    "revision": "651fa2a1ce7161d2ec4e",
+    "url": "/React-TodoApp/static/js/runtime-main.8af19b9b.js"
+  }
+]);
